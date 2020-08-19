1c13ce43b816d41afd3486e274fb13d8869a55c2
amontalvof <amontalvof92@gmail.com>
2020-08-18 19:19:55 -0500

    docs agregados

---
+++ docs/precache-manifest.e2547566b1572c5bb006964097601f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e23d9e02d007be4757dba46960fef37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7623ae3e6343a28a36dd",
+    "url": "/static/css/main.c004df72.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7623ae3e6343a28a36dd",
+    "url": "/static/js/main.6e1d06ca.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
